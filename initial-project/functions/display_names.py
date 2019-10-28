7da6a346839a0790b29da74d55840b1feee2a845
bhokumar <bhoopendra.akgec@gmail.com>
2019-10-28 06:55:32 +0530

    Adding Sets

---
+++ initial-project/functions/display_names.py
@@ -0,0 +1,8 @@
+def display_names(first, second):
+    print(f"{first} says hello to {second}")
+
+names = {"first": "Colt", "second": "Rusty"}
+
+display_names(first="Charlie", second="Hi")
+
+display_names(**names)
---
+++ initial-project/functions/fav_colors.py
@@ -0,0 +1,6 @@
+def fav_colors(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+fav_colors(colt="abc", ruby="red", ethel="teal")
+
